fdbf409c6ef7d4a42ab47f65a0a64f5c20346561
Adam <adam.m.leis@gmail.com>
2018-03-21 18:14:33 -0400

    add starting Scheduler class

---
+++ src/scheduler.js
@@ -0,0 +1,11 @@
+export default class Scheduler {
+    constructor({techees, dateRange}) {
+        this.counter = {};
+        this.techees = {};
+        this.counter = {};
+    }
+
+    resetCounter() {
+        this.counter = {}; // use names
+    }
+}
